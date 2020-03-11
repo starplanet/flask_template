b619d17e74c3b710da3ba11c6347d69866c1ad17
zhangjinjie <zhangjinjie@yimian.com.cn>
2020-03-11 16:02:28 +0800

    fix: fix description failed to render in the default format of reStructuredText

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
 
     description='Flask Project Template',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/',
