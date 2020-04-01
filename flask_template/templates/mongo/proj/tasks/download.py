c1657907f5a681ea7c6e3343a00be5a68d9d0d88
fengao <fengao@yimian.com.cn>
2020-04-01 11:46:00 +0800

    style: formate code

--- flask_template/templates/mongo/proj/tasks/download.py
+++ flask_template/templates/mongo/proj/tasks/download.py
@@ -3,8 +3,9 @@ import requests
 from proj.extensions import celery
 from proj.utils.mail import send_task_status_email
 
+
 @celery.task
-def download_result(download_url,email):
+def download_result(download_url, email):
     result_file = 'result_file' + '.tar.gz'
     with requests.get(download_url, stream=True) as r:
         r.raise_for_status()
@@ -12,4 +13,4 @@ def download_result(download_url,email):
             for chunk in r.iter_content(chunk_size=2048):
                 if chunk:
                     f.write(chunk)
-    send_task_status_email(status='download success',email=email)
+    send_task_status_email(status='download success', email=email)
